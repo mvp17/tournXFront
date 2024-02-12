2f8a97c06c4989f86ad28d09c7dc350230631238
mvp17 <mvisapascual@gmail.com>
2024-02-12 23:27:57 +0100

    Refactor guards' routes.

--- src/plugins/router/routes.ts
+++ src/plugins/router/routes.ts
@@ -1,15 +1,19 @@
+import { authGuard, tournamentMasterGuard, playerGuard } from "./guards";
+
 export const playerRoutes = [
     {
         path: '/team-invitations',
         name: 'Team invitations',
         icon: 'mdi-account-group',
         component: () => import("../../modules/teamInvitations/views/TeamInvitations.vue"),
+        beforeEnter: [authGuard, playerGuard]
     },
     {
         path: '/teams',
         name: 'Teams',
         icon: 'mdi-account-group',
         component: () => import("../../modules/teams/views/Teams.vue"),
+        beforeEnter: [authGuard, playerGuard]
     }
 ];
 
@@ -19,24 +23,28 @@ export const tournamentMasterRoutes = [
       name: 'Tournaments',
       icon: 'mdi-tournament',
       component: () => import("../../modules/tournaments/views/Tournaments.vue"),
+      beforeEnter: [authGuard, tournamentMasterGuard]
     },
     {
       path: '/tournament-invitations',
       name: 'Tournament invitations',
       icon: 'mdi-tournament',
       component: () => import("../../modules/tournamentInvitations/views/TournamentInvitations.vue"),
+      beforeEnter: [authGuard, tournamentMasterGuard]
     },
     {
       path: '/rounds',
       name: 'Rounds',
       icon: 'mdi-recycle-variant',
       component: () => import("../../modules/rounds/views/Rounds.vue"),
+      beforeEnter: [authGuard, tournamentMasterGuard]
     },
     {
       path: "/matches",
       name: "Matches",
       icon: "mdi-soccer-field",
       component: () => import("../../modules/matches/views/Matches.vue"),
+      beforeEnter: [authGuard, tournamentMasterGuard]
     },
 ];
 
@@ -46,12 +54,14 @@ export const commonRoutes = [
       name: 'Home',
       icon: 'mdi-home',
       component: () => import("../../modules/home/views/Home.vue"),
+      beforeEnter: authGuard
     },
     {
       path: '/match-results',
       name: 'Match Results',
       icon: 'mdi-soccer-field',
       component: () => import("../../modules/matchResults/views/MatchResults.vue"),
+      beforeEnter: authGuard
     },
 ];
 
